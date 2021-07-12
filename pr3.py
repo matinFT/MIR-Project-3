bfe4bc8890a59c3aec3dc9bd811b0c83e70af24d
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2021-07-12 12:07:10 +0430

    added collaborative filtering. changed a bit about crawling and crawled papers again

--- pr3.py
+++ pr3.py
@@ -4,7 +4,6 @@ import json
 import numpy as np
 import pandas as pd
 import math
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_article_info(driver):
@@ -22,10 +21,9 @@ def get_article_info(driver):
     year = driver.find_element_by_css_selector("div.name-section > a > span.year").text
     info["date"] = int(year)
 
-    authors = driver.find_element_by_css_selector("div.name-section > ma-author-string-collection > " +
-                                                  "div.au-target > div.authors").text.split(" , ")
+    authors = [x.text for x in driver.find_elements_by_css_selector("div.name-section > ma-author-string-collection > " +
+                                                    "div.au-target > div.authors > div.author-item > a.author")]
     info["authors"] = authors
-
     click_button_element = driver.find_element_by_css_selector(
         "div.name-section > div.topics > ma-tag-cloud > div > div.show-more")
     driver.execute_script("arguments[0].click();", click_button_element)
@@ -123,18 +121,22 @@ def save_page_rank(alpha, papers_file):
         paper_to_index[papers_data[index]["id"]] = index
 
     adj_matrix = cal_adj_matrix(papers_data, paper_to_index)
-    p = adj_matrix.copy()
-    v = np.zeros(len(p[0]))
-    v.fill(1 / len(p[0]))
-    for i in range(len(p)):
-        if sum(p[i]) == 0:
-            p[i] = v
+    P = adj_matrix.copy()
+    V = np.zeros(len(P[0]))
+    V.fill(1 / len(P[0]))
+    V = np.matrix(V)
+    #     return V
+    for i in range(len(P)):
+        if sum(P[i]) == 0:
+            P[i] = V
         else:
-            p[i] = (1 - alpha) * p[i] + alpha * v
-
-    res = v.reshape((1, len(v))).dot(p)
-    for i in range(10):
-        res = res.dot(p)
+            P[i] = P[i] / sum(P[i])
+            P[i] = (1 - alpha) * P[i] + alpha * V
+    #     return P
+    P = np.matrix(P)
+    res = V * P
+    for i in range(1000):
+        res = res * P
 
     with open("PageRank.json", "w") as page_rank_file:
         json.dump(dict([(i, res[0, paper_to_index[i]]) for i in paper_to_index]), page_rank_file)
@@ -209,24 +211,114 @@ def cal_papers_profiles(papers_data, topics):
     return papers_profiles
 
 
-papers_file_name = "content.json"
+def corr(a, b):
+    a_tilda = a - np.mean(a)
+    b_tilda = b - np.mean(b)
+    return a_tilda.dot(b_tilda) / (np.sqrt(sum(a_tilda ** 2)) * np.sqrt(sum(b_tilda ** 2)))
 
-save_papers(2000)
 
-# with open(papers_file_name, "r") as f:
-#     page_rank_file = save_page_rank(0.5, f)
-#     f.close()
+def pearson_similarity(a, b):  # a,b as np.array
+    co_rated_items = (a != 0) * (b != 0)
+    a_temp = a[co_rated_items]
+    b_temp = b[co_rated_items]
+    return corr(a_temp, b_temp)
 
-# with open(papers_file_name, "r") as f:
-#     top_writers = cal_hits_top_writers(10, f)
-#     print(top_writers)
-#     f.close()
-#     problem with writer with '' name!
 
-# user_profiles = pd.read_csv("data.csv")
-# user_profiles.fillna(0, inplace=True)
-# topics = list(user_profiles.columns)
-# with open(papers_file_name, "r") as f:
-#     papers_profiles = cal_papers_profiles(json.load(f), topics)
-#     f.close()
-# print(recommend_paper(np.array(user_profiles.loc[1, :]), papers_profiles))
+# returns top n profiles with their similarity
+# user_profiles, user_prof as np.array
+def close_user_profiles(user_prof, user_profiles, n, predict_index):
+    rated_fields = list(user_prof != 0)
+    rated_fields[predict_index] = True
+    scores = {}
+    for i in range(len(user_profiles)):
+        if 0 not in user_profiles[i, rated_fields]:
+            scores[i] = pearson_similarity(user_prof, user_profiles[i])
+    return [(user_profiles[x[0]], x[1]) for x in sorted(scores.items(),
+                                                        key=lambda x: x[1], reverse=True)[:n]]
+
+
+# puts user mean if no similar user found
+def collaborative_filter(user_profile, n):
+    user_profiles = pd.read_csv("data.csv")
+    user_profiles.fillna(0, inplace=True)
+    user_prof = np.array(user_profile).copy()
+    user_mean = np.mean(user_profile)
+    for i in range(len(user_prof)):
+        if user_profile[i] == 0:
+            top_similar_users = close_user_profiles(np.array(user_profile), np.array(user_profiles), n, i)
+            user_prof[i] = user_mean
+            s = 0
+            if top_similar_users:
+                for x in top_similar_users:
+                    s += x[1] * (x[0][i] - np.mean(x[0]))
+                user_prof[i] += s / sum([x[1] for x in top_similar_users])
+    user_prof = user_prof / sum(user_prof)
+
+    with open(papers_file_name, "r") as f:
+        papers_profiles = cal_papers_profiles(json.load(f), list(user_profiles.columns))
+        f.close()
+    return user_prof, recommend_paper(user_prof, papers_profiles)
+
+
+papers_file_name = "CrawledPapers.json"
+while True:
+    try:
+        a = int(input("enter '1' for starting the crawler\nenter '2' to calculate and save pageRank\n" +
+                      "enter '3' for calculating top writers\nenter '4' to recommend papers\n" +
+                      "enter '5' to estimate user rates and recommend papers\n"))
+    except:
+        print("enter a number")
+        continue
+
+    if a == 1:
+        save_papers(2000)
+
+    elif a == 2:
+        with open(papers_file_name, "r") as f:
+            page_rank_file = save_page_rank(0.5, f)
+            f.close()
+            page_rank_file.close()
+        print("pageRank saved in PageRank.json\n")
+
+    elif a == 3:
+        with open(papers_file_name, "r") as f:
+            top_writers = cal_hits_top_writers(10, f)
+            f.close()
+        print(pd.DataFrame({"writer": [x[0] for x in top_writers],
+                            "score": [x[1] for x in top_writers]}))
+        print("\n")
+
+    elif a == 4:
+        user_profiles = pd.read_csv("data.csv")
+        user_profiles.fillna(0, inplace=True)
+        topics = list(user_profiles.columns)
+        while True:
+            try:
+                user_index = int(input("enter user's index whom you want to recommend\n"))
+                break
+            except:
+                continue
+        with open(papers_file_name, "r") as f:
+            papers_profiles = cal_papers_profiles(json.load(f), topics)
+            f.close()
+        print(recommend_paper(np.array(user_profiles.loc[user_index, :]), papers_profiles))
+        print("\n")
+
+    elif a == 5:
+        while True:
+            try:
+                user_index = int(input("enter user's index whom you want to collaborative_filter\n"))
+                break
+            except:
+                continue
+        user_profiles = pd.read_csv("data.csv")
+        user_profiles.fillna(0, inplace=True)
+        pred_profile, recommended = collaborative_filter(user_profiles.iloc[user_index, :], 10)
+        print(pd.DataFrame({
+            "before": list(user_profiles.iloc[user_index, :]),
+            "after": list(pred_profile)
+        }))
+        print(recommended)
+        print("\n")
+    else:
+        print("enter an integer between 1 and 5")
